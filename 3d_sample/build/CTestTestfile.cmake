a216ad17cfe8edd99e6484145cedf9a255212760
Yu Jing <yujing5b5d@gmail.com>
2014-09-28 23:22:09 +0800

    remove build file

--- 3d_sample/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yu/Workspace/res/script/c/opengl/hello3d
-# Build directory: /Users/yu/Workspace/res/script/c/opengl/hello3d/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(include)
-subdirs(src)
-subdirs(lib)
